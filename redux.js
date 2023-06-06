b6d55ab013a3fbcb3ef316202fac87791ffe9496
softRahul10 <softrahul10@gmail.com>
2023-06-07 01:39:51 +0530

    Create a reducer function with  initial state object and some actions

--- redux.js
+++ redux.js
@@ -1,5 +1,25 @@
 //1. import redux
 const redux = require('redux');
 
+const defaultState = {
+    count:0
+}
+
+const countReducer = (state = defaultState, action) => {
+    if(action.type === 'Increment') {
+        return {
+            count: state.count + 1
+        }
+    }
+
+    if(action.type === 'Decrement') {
+        return {
+            count: state.count - 1
+        }
+    }
+
+    return state;
+};
+
 //2. create redux store using createStore function
-const store = redux.createStore();
+const store = redux.createStore(countReducer);
